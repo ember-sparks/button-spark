e0ef3223d83d638484eedc041dd14e5ab63c2d8e
Tristan Edwards <tristan.edwards@me.com>
2017-01-23 18:35:28 +0100

    Change rootURL in prod

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -52,7 +52,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/ember-button-spark/';
+    ENV.rootURL = '/button-spark/';
 
   }
 
